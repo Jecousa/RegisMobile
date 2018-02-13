b1adda8fb5b681ed871eb2eb6dd18560276677b8
Jakob Essig <jecolousa@gmail.com>
2018-02-12 20:54:17 -0700

    File transfer to add photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+//Pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Client } from '../pages/client/client';
-import {  ClientOrder } from '../pages/client-order/client-order';
+import { ClientOrder } from '../pages/client-order/client-order';
 import { Photographer } from '../pages/photographer/photographer';
 import { EditAlbum } from '../pages/edit-album/edit-album';
+
+//FileTransfer Plugins
+import { File } from '@ionic-native/file';
+import { Transfer } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Camera } from '@ionic-native/camera';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -35,6 +43,10 @@ import { EditAlbum } from '../pages/edit-album/edit-album';
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    Transfer,
+    Camera,
+    FilePath,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
